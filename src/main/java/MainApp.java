6f8e60404fa29bbc45743a27e3064c2f7f0b3374
Srinivasa Reddy B <srinivasabreddy1989@gmail.com>
2025-11-18 14:05:13 +0530

    adding runnable file to java

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,7 @@
+public class MainApp {
+    public static void main(String[] args) {
+        MessageUtil messageUtil = new MessageUtil("Robert");
+        messageUtil.printMessage();
+        messageUtil.salutationMessage();
+    }
+}
